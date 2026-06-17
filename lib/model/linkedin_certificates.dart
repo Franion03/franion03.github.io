1a236896c55867435a76a9b89f181d4900d113d4
Franion03 <64858535+Franion03@users.noreply.github.com>
2026-06-17 22:55:50 +0000

    Automated update of portfolio data

--- lib/model/linkedin_certificates.dart
+++ lib/model/linkedin_certificates.dart
@@ -4,43 +4,15 @@ List<CertificateModel> linkedinCertificates = [
   CertificateModel(
     name: 'AWS Certified Cloud Practitioner (CLF-C02)',
     organization: 'Amazon Web Services (AWS)',
-    date: 'DEC 2025',
-    skills: 'AWS . Cloud Architecture . Infrastructure',
+    date: 'JAN 2026',
+    skills: '',
     credential: 'https://www.credly.com/badges/',
-  ),
-  CertificateModel(
-    name: 'AWS Certified AI Practitioner (AIF-C01)',
-    organization: 'Amazon Web Services (AWS)',
-    date: 'JUN 2026',
-    skills: 'AI/ML . Generative AI . Cloud . MLOps',
-    credential: 'https://www.credly.com/badges/',
-  ),
-  CertificateModel(
-    name: 'Versa Secure SD-WAN Configuration and Administration (V22)',
-    organization: 'Versa Networks Academy',
-    date: '2024',
-    skills: 'SD-WAN . Networking . Security . Routing',
-    credential: '',
-  ),
-  CertificateModel(
-    name: 'Versa Security Service Edge (SSE) with Concerto',
-    organization: 'Versa Networks Academy',
-    date: '2024',
-    skills: 'SSE . SASE . Zero Trust . Network Security',
-    credential: '',
-  ),
-  CertificateModel(
-    name: 'Versa SASE Essentials (ACE)',
-    organization: 'Versa Networks Academy',
-    date: '2024',
-    skills: 'SASE . SD-WAN . Cloud Security . Networking',
-    credential: '',
   ),
   CertificateModel(
     name: 'Computer Science Degree',
     organization: 'University Miguel Hernandez',
     date: 'MAY 2024',
-    skills: 'Algorithms . Data Structures . AI . Networking',
+    skills: '',
     credential: '',
-  ),
+  )
 ];
