e42593d4bd47330f44201b7832c04cadd2c6946a
Franion03 <64858535+Franion03@users.noreply.github.com>
2026-06-19 13:30:28 +0000

    Automated update of portfolio data

--- lib/model/linkedin_certificates.dart
+++ lib/model/linkedin_certificates.dart
@@ -4,9 +4,37 @@ List<CertificateModel> linkedinCertificates = [
   CertificateModel(
     name: 'AWS Certified Cloud Practitioner (CLF-C02)',
     organization: 'Amazon Web Services (AWS)',
-    date: 'JAN 2026',
+    date: 'DEC 2025',
     skills: '',
     credential: 'https://www.credly.com/badges/',
+  ),
+  CertificateModel(
+    name: 'AWS Certified AI Practitioner (AIF-C01)',
+    organization: 'Amazon Web Services (AWS)',
+    date: 'JUN 2026',
+    skills: '',
+    credential: 'https://www.credly.com/badges/',
+  ),
+  CertificateModel(
+    name: 'Versa Secure SD-WAN Configuration and Administration (V22)',
+    organization: 'Versa Networks Academy',
+    date: 'JAN 2024',
+    skills: '',
+    credential: '',
+  ),
+  CertificateModel(
+    name: 'Versa Security Service Edge (SSE) with Concerto',
+    organization: 'Versa Networks Academy',
+    date: 'JAN 2024',
+    skills: '',
+    credential: '',
+  ),
+  CertificateModel(
+    name: 'Versa SASE Essentials (ACE)',
+    organization: 'Versa Networks Academy',
+    date: 'JAN 2024',
+    skills: '',
+    credential: '',
   ),
   CertificateModel(
     name: 'Computer Science Degree',
