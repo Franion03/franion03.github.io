d8662bfbe7161ad4545fc6604fec3572621dadfc
Franion03 <64858535+Franion03@users.noreply.github.com>
2026-06-19 13:26:59 +0000

    Automated update of portfolio data

--- lib/model/linkedin_certificates.dart
+++ lib/model/linkedin_certificates.dart
@@ -1,3 +1,18 @@
 import 'certificate_model.dart';
 
-List<CertificateModel> linkedinCertificates = [];
+List<CertificateModel> linkedinCertificates = [
+  CertificateModel(
+    name: 'AWS Certified Cloud Practitioner (CLF-C02)',
+    organization: 'Amazon Web Services (AWS)',
+    date: 'JAN 2026',
+    skills: '',
+    credential: 'https://www.credly.com/badges/',
+  ),
+  CertificateModel(
+    name: 'Computer Science Degree',
+    organization: 'University Miguel Hernandez',
+    date: 'MAY 2024',
+    skills: '',
+    credential: '',
+  )
+];
